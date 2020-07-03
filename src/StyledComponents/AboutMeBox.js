a68104d4bbc03340aaf61890ef97c2f75e626724
Seb Smith <seb-smith@live.com>
2020-07-03 09:42:34 +0100

    Adds AboutMeBox

--- src/StyledComponents/AboutMeBox.js
+++ src/StyledComponents/AboutMeBox.js
@@ -2,20 +2,21 @@ import React from 'react';
 import styled from 'styled-components';
 import getDateString from 'utils/utils.js'
 
-const Root = styled.p`
+const Root = styled.article`
   font-size: 24px;
   background-color: #3aafa9;
-  margin-top: 3px;
+  margin-top: 2px;
   & > span {
     font-weight: bold;
-    margin-left: 45%;
-    color: #17252A;
-    text-decoration:
+    margin: 0 40% 0 40%;
+    color: #FEFFFF;
+    text-decoration: underline;
+    font-size: 28px;
   }
   & > p {
-    margin: 10px 25% 0 25%;
-    font-size: 26px;
-    color: #FEFFFF;
+    margin: 10px 20% 0 20%;
+    font-size: 28px;
+    color: #17252A;
   }
 `;
 
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,6 +5,7 @@ export default function AboutMe() {
   return (
     <AboutMeBox>
       Hi Im Seb! I am a Junior Software Developer, based near Manchester in the United Kingdom, with a passion for Front End web development.
+      With having recently graduated Northcoders Development Bootcamp, I am hungry to get out into the working world and to add value to a company.
     </AboutMeBox>
   )
 }
